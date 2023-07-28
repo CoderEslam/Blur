804f4ae0abb9563db50faee75c7c5f88478900ba
eslam <eslamghazy600@gmail.com>
2023-07-28 22:49:57 +0300

    Initial commit

---
+++ blurry/src/main/java/com/doubleclick/blurry/BlurFactor.java
@@ -0,0 +1,17 @@
+package com.doubleclick.blurry;
+
+import android.graphics.Color;
+
+
+
+class BlurFactor {
+
+  public static final int DEFAULT_RADIUS = 25;
+  public static final int DEFAULT_SAMPLING = 1;
+
+  public int width;
+  public int height;
+  public int radius = DEFAULT_RADIUS;
+  public int sampling = DEFAULT_SAMPLING;
+  public int color = Color.TRANSPARENT;
+}
